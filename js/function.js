3604fa7079624e8e16805928be4db0773f569790
10EastSea <xgf6580@gmail.com>
2022-03-23 00:38:33 +0900

    select img color

--- js/function.js
+++ js/function.js
@@ -1,3 +1,10 @@
 /* algorithm function */
+function colorSelect(id) {
+    var selectId = $("#" + id);
+    // console.log(selectId.val());
+    
+    if(id == "select_top_clothes") { $("#top_clothes").css("background-color", selectId.val()); }
+    else if(id == "select_bottom_clothes") {}
+}
 
 /* test function */
