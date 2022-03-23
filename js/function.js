0cb940503e8f6a47ed130862664d806dadd1ef9d
10EastSea <xgf6580@gmail.com>
2022-03-23 15:26:12 +0900

    change img + add bottom

--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@ function colorSelect(id) {
     // console.log(selectId.val());
     
     if(id == "select_top_clothes") { $("#top_clothes").css("background-color", selectId.val()); }
-    else if(id == "select_bottom_clothes") {}
+    else if(id == "select_bottom_clothes") { $("#bottom_clothes").css("background-color", selectId.val()); }
 }
 
 /* test function */
