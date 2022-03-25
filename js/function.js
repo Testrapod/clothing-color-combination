f70dee1d490ff615ad6677a792eb648fd0845ec1
10EastSea <xgf6580@gmail.com>
2022-03-25 16:39:33 +0900

    add color picker

--- js/function.js
+++ js/function.js
@@ -1,10 +1,26 @@
 /* algorithm function */
+function colorParser(color) {
+    if(color == "white") return "#FFFFFF";
+    else if(color == "red") return "#FF0000";
+    else if(color == "yellow") return "#FFFF00";
+    else if(color == "green") return "#008000";
+    else if(color == "blue") return "#0000FF";
+}
+
 function colorSelect(id) {
     var selectId = $("#" + id);
     // console.log(selectId.val());
-    
-    if(id == "select_top_clothes") { $("#top_clothes").css("background-color", selectId.val()); }
-    else if(id == "select_bottom_clothes") { $("#bottom_clothes").css("background-color", selectId.val()); }
+
+    if(id == "select_top_color") {
+        if(selectId.val() == "color") { return; }
+        $("#top_clothes").css("background-color", selectId.val());
+        $("#top_color_picker").val(colorParser(selectId.val()));
+    }
+    else if(id == "select_bottom_color") {
+        if(selectId.val() == "color") { return; }
+        $("#bottom_clothes").css("background-color", selectId.val());
+        $("#bottom_color_picker").val(colorParser(selectId.val()));
+    }
 }
 
 /* test function */
--- js/index_ready.js
+++ js/index_ready.js
@@ -1,4 +1,11 @@
 /* index.html document ready */
 $(document).ready(function () {
-    
+    $("#top_color_picker").change(function() {
+        $("#top_clothes").css("background-color", $("#top_color_picker").val());
+        $("#select_top_color").val("color").prop("selected", true);
+    });
+    $("#bottom_color_picker").change(function() {
+        $("#bottom_clothes").css("background-color", $("#bottom_color_picker").val());
+        $("#select_bottom_color").val("color").prop("selected", true);
+    });
 });
