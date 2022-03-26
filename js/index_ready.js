d563ce3d8605c859b3818cdd49ae2b7a1746effc
10EastSea <xgf6580@gmail.com>
2022-03-27 01:00:14 +0900

    add addClothesData()

--- js/index_ready.js
+++ js/index_ready.js
@@ -44,11 +44,67 @@ function settingClothesDatas() {
     });
 }
 
+function addClothesData(clothes_color) {
+    var check = true;
+    
+    $("#clothes_db_table>tbody tr").each(function() {
+        var tr = $(this);
+        var td = tr.children();
+
+        var tmp_clothes = td.eq(1).text();
+        var tmp_color = td.eq(2).text();
+        var tmp_clothes_color = tmp_clothes+"_"+tmp_color;
+    
+        if(tmp_clothes_color == clothes_color) {
+            check = false;
+            return false;
+        }
+    });
+
+    if(check) {
+        clothes_color = clothes_color.split('_');
+        var clothes = clothes_color[0];
+        var color = clothes_color[1];
+
+        var tagContent =
+            '<tr onclick="getClothes(this);">' +
+                '<td><input class="form-check-input" type="checkbox" name="check_list"></td>' +
+                '<td>' + clothes +'</td>' +
+                '<td>' + color + '</td>' +
+            '</tr>';
+
+        $("#clothes_db_table>tbody").append(tagContent);
+
+        if($("#clothes_db_table>tbody tr").length > 0) {
+            $("#clothes_db_table").show();
+            $("#comb_button").show();
+            $("#delete_button").show();
+        } else {
+            $("#clothes_db_table").hide();
+            $("#comb_button").hide();
+            $("#delete_button").hide();
+        }
+
+        $("#total_clothes_num").text(Object.keys(clothes_db).length);
+        $("#all_check").prop("checked", false);
+
+        // check_list
+        $("input:checkbox[name=check_list]").click(function() {
+            $("#selected_clothes_num").text($("input:checkbox[name=check_list]:checked").length);
+            if($("input:checkbox[name=check_list]:checked").length == Object.keys(clothes_db).length) {
+                $("#all_check").prop("checked", true);
+            } else {
+                $("#all_check").prop("checked", false);
+            }
+        });
+    }
+}
+
 $(document).ready(function () {
     for(var i=0, len=localStorage.length; i<len; i++) {
         var key = localStorage.key(i);
         var value = localStorage.getItem(localStorage.key(i));
-        
+
         if(key == value) clothes_db[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
     }
     settingClothesDatas();
@@ -80,16 +136,20 @@ $(document).ready(function () {
 
         if(topType != "default") {
             var item = topType + "_" + $("#add_top_color_picker").val();
+
             localStorage.setItem(item, item);
             clothes_db[item] = item;
+
+            addClothesData(item);
         }
         if(bottomType != "default") {
             var item = bottomType + "_" + $("#add_bottom_color_picker").val();
+
             localStorage.setItem(item, item);
             clothes_db[item] = item;
-        }
 
-        settingClothesDatas();
+            addClothesData(item);
+        }
     });
 
 
